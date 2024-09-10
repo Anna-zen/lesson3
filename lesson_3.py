200e335f691248895f001f4f0ab5973ca3855a87
Anna Zen <annadavidson7777@gmail.com>
2024-09-10 23:55:11 +0300

    Add files via upload

---
+++ lesson_3.py
@@ -0,0 +1,6 @@
+#Практическое задание по работе в Pycharm - "Переменные"
+number_of_homeworks = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_work = hours_spent / number_of_homeworks
+print ("Курс:",course_name,', всего задач:',number_of_homeworks,', затрачено часов:', hours_spent, ', среднее время выполнения', time_for_one_work, 'часа')
